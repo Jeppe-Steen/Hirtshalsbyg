04dc31c2100306e708f64c96dd11f4ad11b85bc0
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2024-04-26 20:56:48 +0200

    create git repo

---
+++ app.vue
@@ -0,0 +1,92 @@
+<script setup>
+const handleCall = () => {
+  window.open("tel:+4528727599");
+  return;
+}
+</script>
+<template>
+  <div class="page-wrapper">
+    <section class="page-hero hero">
+      <header class="hero__header">
+        <h1 class="hero__heading uppercase">Hirtshals byg</h1>
+        <p class="hero__subheading" @click="handleCall()">Tlf: 28 72 75 99</p>
+        <p class="hero__text">Hirtshals byg står klar til at hjælpe dig med dit næste projekt. <br> Så tøv endelig ikke med at kontakte os hvis du står og skal have hjælp, vi er klar til at hjælpe med store som små opgaver!</p>
+        <button class="btn btn--outlined white hide" type="button">
+          Se billeder her
+        </button>
+
+        <Arrow/>
+      </header>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+
+.uppercase {
+  text-transform: uppercase;
+}
+
+.page-wrapper{
+  height: 100vh;
+  width: 100%;
+  background-color: #333;
+
+  .hero {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &__header {
+      width: 50%;
+      text-align: center;
+      color: white;
+    }
+
+    &__heading {
+      font-size: 50px;
+      text-shadow: 3px 3px 5px #00000050;
+    }
+    &__subheading {
+      font-size: 30px;
+      cursor: pointer;
+    }
+
+    &__text {
+      margin-top: 20px;
+    }
+  }
+}
+
+.btn {
+  padding: 10px 20px;
+  margin: 20px;
+  font-size: 18px;
+  border-radius: 6px;
+
+  &--outlined {
+    background: none;
+    border: 1px solid #fff;
+  }
+
+ &:hover {
+  cursor: pointer;
+  box-shadow: 0px 4px 8px #00000050;
+ }
+}
+
+.white {
+  color: white;
+}
+
+.hide {
+  display: none !important;
+}
+</style>
---
+++ components/arrow.vue
@@ -0,0 +1,48 @@
+<script setup>
+    const props = defineProps({
+        show: {
+            type: Boolean,
+            default: false,
+        }
+    })
+
+</script>
+
+<template>
+    <div class="arrow-container" :class="{show: !show}">
+        <div class="arrow">
+            <svg fill="#fff" transform="rotate(-90)" viewBox="0 0 330 330">
+                <path id="XMLID_92_" d="M111.213,165.004L250.607,25.607c5.858-5.858,5.858-15.355,0-21.213c-5.858-5.858-15.355-5.858-21.213,0.001
+                    l-150,150.004C76.58,157.211,75,161.026,75,165.004c0,3.979,1.581,7.794,4.394,10.607l150,149.996
+                    C232.322,328.536,236.161,330,240,330s7.678-1.464,10.607-4.394c5.858-5.858,5.858-15.355,0-21.213L111.213,165.004z"/>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.arrow-container{
+    width: 40px;
+    position: absolute;
+    left: 50%;
+    bottom: 100px;
+    transform: translateX(-50%);
+
+    .arrow {
+        animation: example .9s linear infinite;
+    }
+}
+
+@keyframes example {
+    0% {
+        transform: translateY(0px);
+    }
+    100% {
+        transform: translateY(20px);
+    }
+}
+
+.show {
+    display: none;
+}
+</style>
